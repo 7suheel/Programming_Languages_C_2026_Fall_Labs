7154e79ae6a751ae7490e33a73300bfc346ef3e8
Suheyl <137324968+7suheel@users.noreply.github.com>
2026-09-24 12:14:22 +0000

    Lab 2 completed

--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,8 +12,11 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+    long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 int main(void) {
@@ -22,7 +25,12 @@ int main(void) {
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
 
-    // TODO: validate input, call function, print result
+        if (n < 0) {
+        printf("Error: n must not be negative.\n");
+        return 1;
+    }
+
+    printf("%d! = %lld\n", n, factorial(n));
 
     return 0;
 }
